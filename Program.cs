8073619b5ad350106c35c3168069825ec3619506
shanedbutler <shanedbutler@gmail.com>
2022-09-19 11:53:42 -0400

    Set properties in constructor

--- Program.cs
+++ Program.cs
@@ -8,18 +8,24 @@
             Fantatatown.Mayor = "Claire";
             Fantatatown.Establish();
 
-            Building FifteenTenWest = new Building("1510 West Avenue");
-            FifteenTenWest.Stories = 3;
-            FifteenTenWest.Width = 150;
-            FifteenTenWest.Depth = 200;
+            Building FifteenTenWest = new Building("1510 West Avenue")
+            {
+                Stories = 3,
+                Width = 150,
+                Depth = 200
+            };
+
             FifteenTenWest.Construct();
             FifteenTenWest.Purchase("Diego");
             Fantatatown.AddBuilding(FifteenTenWest);
 
-            Building OneZeroThreeGates = new Building ("103 Gates Ave");
-            OneZeroThreeGates.Stories = 4;
-            OneZeroThreeGates.Width = 75;
-            OneZeroThreeGates.Depth = 125;
+            Building OneZeroThreeGates = new Building("103 Gates Ave")
+            {
+                Stories = 4,
+                Width = 75,
+                Depth = 125
+            };
+            
             OneZeroThreeGates.Construct();
             OneZeroThreeGates.Purchase("Fernando");
             Fantatatown.AddBuilding(OneZeroThreeGates);
